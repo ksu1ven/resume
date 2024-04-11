954dc607469c65e40ed680290005c14f34fa1eb4
Oksana <ksu1ven@mail.ru>
2024-04-11 15:01:57 +0300

    feat: add readystate for preloaders

--- src/components/layout/Header/blocks/CatBanana.tsx
+++ src/components/layout/Header/blocks/CatBanana.tsx
@@ -138,10 +138,14 @@ export function CatBanana({ isCatAnimated }: CatBananaProps) {
                 )}
             </AnimatePresence>
             <audio src={happySong} ref={happySongRef} muted />
-            <link rel="preload" as="image" href={catBananaLeft} />
-            <link rel="preload" as="image" href={catBananaRight} />
-            <link rel="preload" as="image" href={mouseCursorLeft} />
-            <link rel="preload" as="image" href={mouseCursorRight} />
+            {document.readyState === 'complete' && (
+                <>
+                    <link rel="preload" as="image" href={catBananaLeft} />
+                    <link rel="preload" as="image" href={catBananaRight} />
+                    <link rel="preload" as="image" href={mouseCursorLeft} />
+                    <link rel="preload" as="image" href={mouseCursorRight} />
+                </>
+            )}
         </div>
     );
 }
