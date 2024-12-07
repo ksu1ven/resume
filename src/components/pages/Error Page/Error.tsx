8b58fdb39591008bd9c8568e8460213c802a8cdf
Oksana Yahimovich <101278229+ksu1ven@users.noreply.github.com>
2024-12-07 16:05:33 +0300

    fix error page

--- src/components/pages/Error Page/Error.tsx
+++ src/components/pages/Error Page/Error.tsx
@@ -1,28 +1,22 @@
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import errorCat from '@assets/images/error-cat.png';
 
 export function Error() {
-    useEffect(() => {
-        (document.querySelector('#root') as HTMLElement).style.setProperty(
-            'display',
-            'block'
-        );
-    }, []);
-
     return (
-        <main className="error-bg">
-            <section className="wrapper error">
-                <h1 className="error__h1">Вы свернули куда-то не туда :( </h1>
+        <main className="error">
+            <div className="wrapper error__wrapper">
+                <h1 className="error__title">Вы свернули куда-то не туда :(</h1>
+
                 <Link to="/" className="btn error__link">
                     Главная
                 </Link>
+
                 <img
                     className="error__img"
                     src={errorCat}
                     alt="Error Page Cat"
                 />
-            </section>
+            </div>
         </main>
     );
 }
